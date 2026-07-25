041c270080b5a6df8c4bf3b1a71277b2fb3033d2
Oscar <osm1019@gmail.com>
2026-07-25 12:55:54 -0700

    oplus-fwk/sepolicy: Stub SurfaceExtImpl; Gallery→horae binder
    Add a no-JNI SurfaceExtImpl for Camera preload Class.forName, and
    allow platform_app to binder-call the horae thermal daemon.
    
    Change-Id: Iad1c4765790fc6b3472e66498c6037e46b3ac653

---
+++ oplus-fwk/src/android/view/SurfaceExtImpl.java
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Stub for stock android.view.SurfaceExtImpl (oplus-framework.jar).
+ * Camera preloadClassForDlOpen does Class.forName on this; stock impl also
+ * loads oplusgui_jni and needs Surface wrappers we do not ship.
+ */
+package android.view;
+
+public class SurfaceExtImpl {
+    public SurfaceExtImpl(Object base) {
+    }
+
+    public void setMaxDequeuedBufferCount(int bufferCount) {
+    }
+}
